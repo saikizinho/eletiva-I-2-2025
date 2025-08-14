ae1436ada1862e56a007e04158da467311b082fe
saikizinho <vsaiki50@gmail.com>
2025-08-14 20:02:24 -0300

    Primeiro codigo em php

---
+++ exemplo.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+</head>
+<body>
+    <?php
+        $dia = date("d");
+    ?>
+    <h1> hoje é dia <?= $dia ?> de agosto de 2025</h1>
+</body>
+</html>
