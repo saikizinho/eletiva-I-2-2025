9eee67a901ea1ccb76a3161a845feccbad453dfe
saikizinho <vsaiki50@gmail.com>
2025-08-21 22:06:23 -0300

    aula dia 21-8

---
+++ lista de exercicios/exercicio2.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Exemplo - soma de valores</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container">
+        <h1>Exemplo - subtração de valores</h1>
+        <form method="post">
+            <div class="mb-3">
+                <label for="valor1" class="form-label">informe o primeiro valor</label>
+                <input type="number" id="valor1" name="valor1" class="form-control" required="">
+            </div>
+            <div class="mb-3">
+                <label for="valor2" class="form-label">informe o segundo valor</label>
+                <input type="number" id="valor2" name="valor2" class="form-control" required="">
+            </div>
+            <button type="submit" class="btn btn-primary">Enviar</button>
+        </form>
+
+        <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $valor1 = $_POST['valor1'];
+            $valor2 = $_POST['valor2'];
+            $subtracao = $valor2 - $valor1;
+            echo "<p>A subtração de $valor2 - $valor1 é : $subtracao </p>";
+        }
+        // -  * x**y x/y x//y x%y x<y x>y x<=y x>=y x==y x!=y 
+        ?>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+    </div>
+</body>
+
+</html>
---
+++ lista de exercicios/exercicio6.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Exemplo - soma de valores</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container">
+        <h1>Exemplo - Conversão de temperatura</h1>
+        <form method="post">
+            <form method="post">
+                <div class="mb-3">
+                    <label for="valor1" class="form-label">Informe a temperatura em ºC</label>
+                    <input type="number" id="valor1" name="valor1" class="form-control" required>
+                </div>
+                <button type="submit" class="btn btn-primary">Enviar</button>
+            </form>
+
+            <?php
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                $valor1 = $_POST['valor1'];
+
+                $fahrenheit = ($valor1 * 9 / 5) + 32;
+
+                echo "<p>$valor1 °C é igual a $fahrenheit °F</p>";
+            }
+            ?>
+
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+    </div>
+</body>
+
+</html>
